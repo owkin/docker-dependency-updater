cd91ed58d82819aa361d3ba8ab2ce0178a00f282
Alexandre-P <alexandre.picosson@owkin.com>
2022-02-22 17:35:14 +0100

    feat: check with alpine

---
+++ src/dockerfile.ts
@@ -0,0 +1,21 @@
+import * as image from './image'
+import fs from 'fs'
+
+export function load(dockerfile: string): image.Image {
+  const content = fs.readFileSync(dockerfile).toString('utf-8')
+  return extract_docker_image(content)
+}
+
+function extract_docker_image(dockerfile_content: string): image.Image {
+  let imageName = ''
+  const dockerfileLines = dockerfile_content.split('\n')
+  for (const line of dockerfileLines) {
+    if (line.includes('FROM')) {
+      imageName = line.split(' ')[1].trim()
+    }
+    if (line.includes('apk add') || line.includes('apt-get install')) {
+      return image.factory(imageName)
+    }
+  }
+  throw Error('Unable to extract image from Dockerfile')
+}
---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import * as core from '@actions/core'
+import * as dependencies from './dependencies'
+import * as dockerfile from './dockerfile'
+
+async function run(): Promise<void> {
+  try {
+    const dockerfile_path = core.getInput('dockerfile')
+    const dependencies_path = core.getInput('dependencies')
+
+    const image = dockerfile.load(dockerfile_path)
+    const dependencies_info = dependencies.load(dependencies_path)
+    const updated_dependencies = dependencies_info.map(async function (
+      installed_pkg
+    ) {
+      return image.get_latest_version(installed_pkg)
+    })
+
+    const updated_packages = await Promise.all(updated_dependencies)
+    dependencies.save(dependencies_path, updated_packages)
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+run()
