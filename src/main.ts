e20453bc3df5f6e255b7db4d49c528fc1265a750
Alexandre-P <alexandre.picosson@owkin.com>
2022-02-22 18:01:02 +0100

    refactor: rename variables

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,13 @@ async function run(): Promise<void> {
 
     const image = dockerfile.load(dockerfile_path)
     const dependencies_info = dependencies.load(dependencies_path)
-    const updated_dependencies = dependencies_info.map(async function (
+    const packages_update = dependencies_info.map(async function (
       installed_pkg
     ) {
       return image.get_latest_version(installed_pkg)
     })
 
-    const updated_packages = await Promise.all(updated_dependencies)
+    const updated_packages = await Promise.all(packages_update)
     dependencies.save(dependencies_path, updated_packages)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
