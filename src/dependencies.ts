68fc31b231ac9abafc2522d853d39b8e5645a087
musdotdigital <mustafa.al-quraishi@owkin.com>
2023-11-29 14:46:50 +0000

    fix: tsc dependencies.ts

--- src/dependencies.ts
+++ src/dependencies.ts
@@ -4,14 +4,21 @@ import {factory, Image} from './image'
 export function load(dependencies_path: string): [Image, Package[]] {
   const content = fs.readFileSync(dependencies_path).toString('utf-8')
   const jsonContent = JSON.parse(content)
-  return [factory(jsonContent.image), packages_from_dict(jsonContent.dependencies)]
+  return [
+    factory(jsonContent.image),
+    packages_from_dict(jsonContent.dependencies)
+  ]
 }
 
-export function save(dependencies_path: string, image: string, dependencies: Package[]): void {
+export function save(
+  dependencies_path: string,
+  image: string,
+  dependencies: Package[]
+): void {
   const jsonData = {
-    image: image,
-    dependencies: dependencies
-  };
+    image,
+    dependencies
+  }
   const jsonContent = JSON.stringify(jsonData, null, 2)
   fs.writeFileSync(dependencies_path, jsonContent)
 }
@@ -19,15 +26,17 @@ export function save(dependencies_path: string, image: string, dependencies: Pac
 interface StoredJSON {
   name: string
   version: string
-  [key: string]: string
 }
 
 export class Package {
   name: string
   version: string
-  [key: string]: string
 
-  constructor(name: string, version: string, extraFields?: {[key: string]: string}) {
+  constructor(
+    name: string,
+    version: string,
+    extraFields?: {[key: string]: string}
+  ) {
     this.name = name
     this.version = version
     Object.assign(this, extraFields)
@@ -37,7 +46,9 @@ export class Package {
 function packages_from_dict(dict: StoredJSON[]): Package[] {
   const packages: Package[] = []
   for (const storedPackage of dict) {
-    packages.push(new Package(storedPackage.name, storedPackage.version, {...storedPackage}))
+    packages.push(
+      new Package(storedPackage.name, storedPackage.version, {...storedPackage})
+    )
   }
   return packages
 }
