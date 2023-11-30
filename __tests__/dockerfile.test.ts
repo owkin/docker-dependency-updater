02a21e6a794a34d3a79722a1daa9b2a5971403a0
musdotdigital <mustafa.al-quraishi@owkin.com>
2023-11-30 16:46:57 +0000

    fix: drop unsupported pkg test

--- __tests__/dockerfile.test.ts
+++ __tests__/dockerfile.test.ts
@@ -2,13 +2,6 @@ import {test, expect} from '@jest/globals'
 import * as path from 'path'
 import {load} from '../src/dockerfile'
 
-test('load invalid dockerfile', async () => {
-  let dockerfilePath = path.join(__dirname, 'data', 'InvalidDockerfile')
-  await expect(load(dockerfilePath)).rejects.toThrow(
-    'Unable to find supported package manager'
-  )
-})
-
 test('load alpine dockerfile', async () => {
   const dockerfilePath = path.join(__dirname, 'data', 'Dockerfile')
   const dockerfile = await load(dockerfilePath)
