066792889c0883b0746ec7a33aa0de22ae2b68bc
Alexandre-P <alexandre.picosson@owkin.com>
2026-04-29 11:49:47 +0200

    tests: adjust timeout of dockerfile tests (#548)
    These tests are often failing to complete within the allowed timeframe
    in the CI, giving it more time should solve the issue.
    
    Signed-off-by: Alexandre-P <alexandre.picosson@owkin.com>

--- __tests__/dockerfile.test.ts
+++ __tests__/dockerfile.test.ts
@@ -10,21 +10,21 @@ test('load alpine dockerfile', async () => {
   const dockerfile = await load(dockerfilePath)
   expect(dockerfile.pkgManager).toBe('apk')
   expect(dockerfile.name).toBe('alpine:latest')
-}, 10000)
+}, 60000)
 
 test('load ubuntu dockerfile', async () => {
   const dockerfilePath = path.join(__dirname, 'data', 'Dockerfile.apt')
   const dockerfile = await load(dockerfilePath)
   expect(dockerfile.pkgManager).toBe('apt')
   expect(dockerfile.name).toBe('ubuntu:latest')
-}, 10000)
+}, 60000)
 
 test('load dockerfile with unsupported package manager', async () => {
   const dockerfilePath = path.join(__dirname, 'data', 'Dockerfile.unsupported')
   await expect(load(dockerfilePath)).rejects.toThrow(
     'Unable to find supported package manager'
   )
-}, 10000)
+}, 60000)
 
 test('load multi stage dockerfile', async () => {
   const dockerfilePath = path.join(__dirname, 'data', 'Dockerfile.multi')
